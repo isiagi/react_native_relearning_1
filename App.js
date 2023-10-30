d15d139a0c2e33c84ae38f630c47d275e5390345
isiagi <isiagigeofrey0@gmail.com>
2023-10-30 13:22:28 +0300

    chore: Adds FlatList

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ButtonComponent from "./components/Button";
+import FlatListComponent from "./components/FlatListComponent";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <ButtonComponent />
-    </View>
-  );
+  return <FlatListComponent />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
