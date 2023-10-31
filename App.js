d5dc890c93ad2b1ec1550bfe95f77e54ddd264ab
isiagi <isiagigeofrey0@gmail.com>
2023-10-31 09:49:08 +0300

    chore: Adds RefreshControlComponent

--- App.js
+++ App.js
@@ -6,9 +6,10 @@ import ImageComponent from "./components/Image";
 import ImageBackgroundComponent from "./components/ImageBackgroundComponent";
 import KeyboardAvoidingComponent from "./components/KeyboardAvoidingComponent";
 import ModalComponent from "./components/ModalComponent";
+import RefreshControlComponent from "./components/RefreshControlComponent";
 
 export default function App() {
-  return <ModalComponent />;
+  return <RefreshControlComponent />;
 }
 
 const styles = StyleSheet.create({});
